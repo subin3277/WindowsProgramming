0b94adee819c576a9d817b665b8279e4a8aaa61a
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-09-10 17:03:18 +0900

    week2

---
+++ ch2/ch2/Method.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch2
+{
+    class Method
+    {
+        static void Main(string[] args)
+        {
+            int a = 2;
+            int b = 3;
+            int tot = a + b;
+            Console.WriteLine("a + b = {0}", tot);
+        }
+    }
+}
---
+++ ch2/ch2/데이터형식.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch2
+{
+    class 데이터형식
+    {
+        static void Main(string[] args)
+        {
+            char c1 = 'a'; //소문자
+            string s1 = "aaa"; //키워드
+            bool b1 = true;
+
+            Char C2 = 'B'; //대문자
+            String S2 = "BBB"; //닷넷 데이터형식
+            Boolean B2 = false;
+
+            Console.WriteLine("{0} {1} {2}", c1, s1, b1);
+            Console.WriteLine("{0} {1} {2}", C2, S2, B2);
+        }
+    }
+}
