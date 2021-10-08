5270da31945cd96f04b6d859e704e6d7fc5fa81c
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-10-08 23:36:30 +0900

    week5

---
+++ ch5/ch5/ch5/Method.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch5
+{
+    class Method
+    {
+        public static void Main(string[] args)
+        {
+            Start(); //매개변수, 반환값 없는 메소드
+        }
+
+        private static void Start()
+        {
+            Console.WriteLine("매개변수와 반환 값이 없는 메소드");
+        }
+    }
+}
