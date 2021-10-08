690e16799d0ef7398e9545871a09a4196df015f9
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-10-09 00:36:18 +0900

    week5

--- ch5/ch5/ch5/Method.cs
+++ ch5/ch5/ch5/Method.cs
@@ -11,6 +11,33 @@ namespace ch5
         public static void Main(string[] args)
         {
             Start(); //매개변수, 반환값 없는 메소드
+
+            Hi("매개 변수가 있는 메소드 입니다.");
+
+            int value = Plus(3, 4);
+            Console.WriteLine("3+4={0}",value);
+
+            Print_Out("다른 타입의 매개변수가 여러개 일 때", 3);
+        }
+
+        private static void Print_Out(string v1, int v2)
+        {
+            //throw new NotImplementedException();
+            for(int i=0;i<v2;i++)
+                Console.WriteLine("{0}, {1}",i,v1);
+        }
+
+        private static int Plus(int v1, int v2)
+        {
+            //throw new NotImplementedException();
+            int c = v1 + v2;
+            return c;
+        }
+
+        private static void Hi(string v)
+        {
+            //throw new NotImplementedException();
+            Console.WriteLine(v);
         }
 
         private static void Start()
---
+++ ch5/ch5/ch5/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch5
+{
+    class Person
+    {
+        public static void Main(string[] args)
+        {
+            Man m1 = new Man();
+            Man.name = "KBS";
+            m1.age = 22;
+            m1.weight = 55.5F;
+            Console.WriteLine("{0},{1},{2}",Man.name,m1.age,m1.weight);
+        }
+    }
+
+    class Man
+    {
+        public static string name;
+        public int age;
+        public float weight;
+    }
+}
