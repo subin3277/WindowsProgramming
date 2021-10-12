98f94314dcb98050ac501aeceda06a085dea743a
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-10-12 22:47:02 +0900

    week5

---
+++ ch5/ch5/ch5/Ref_Test.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch5
+{
+    class Ref_Test
+    {
+        static void CallByRef(ref int x)  // 50 이 저장되어 있는 주소의 값은 정수다
+        {
+            Console.WriteLine("함수내로 전달된 값 : {0}", x);
+            x = 1000;     // 전달 받은 주소의 값을 수정
+            Console.WriteLine("함수내 재 지정한 값 : {0}", x);  // 수정된 값 출력
+        }
+
+        public static void Main(string[] args)
+        {
+            int y = 50;
+            CallByRef(ref y);  // ref : 50 넘겨주는 것이 아니라, 50 이 저장된 주소를 전달함
+            Console.WriteLine("Call By Ref : {0}", y);   // 위에서 같은 주소에 수정된 값 출력
+        }   // 외부에서 내부로 값을 전달할 때 : ref 사용
+    }
+}
---
+++ ch5/ch5/ch5/참조형.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch5
+{
+    class kbs
+    {
+        public int a = 5;
+    }
+    class 참조형
+    {
+        static void Main(string[] args)
+        {
+            kbs k1 = new kbs(); //  k1에는 주소값이 전달 – 인스턴스는 주소 값 (1000번지)  
+            kbs k2 = k1;      //  k2 에 k1주소 가 전달  - k1,  k2 같은 번지 공유 1000번지
+
+            Console.WriteLine("k1의 a 값은 {0},  k2의 a 값은 {1}", k1.a, k2.a);
+
+            k2.a = 12;   // 1000번지 데이터를 12로 변환 – k1, k2 둘 다 12 로 수정됨
+            Console.WriteLine("k1의 a 값은 {0},  k2의 a 값은 {1}", k1.a, k2.a);
+        }
+
+    }
+}
