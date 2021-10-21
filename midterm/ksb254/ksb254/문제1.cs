d5446f35f9dde04cb9fe8767458972a015c5cdb1
DESKTOP-FPM3JEV\Administrator <subin3277@naver.com>
2021-10-21 21:11:54 +0900

    midterm

---
+++ midterm/ksb254/ksb254/문제1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ksb254
+{
+    class 문제1
+    {
+        static void Main(string[] args)
+        {
+            int dis = 0;
+            Console.Write("택시로 이동할 거리(KM)를 정수로 입력하세요 : ");
+            dis = Convert.ToInt32(Console.ReadLine());
+
+            Taxi taxi = new Taxi(dis);
+            Console.WriteLine("이동 거리는 {0} KM는 {1} Mile 이고",dis, taxi.MileTransform());
+            Console.WriteLine("택시요금은 {0}원 이다.", taxi.Money());
+            
+        }
+    }
+
+    class Taxi : ICalc
+    {
+        private int _Distance;
+        public Taxi(int distance)
+        {
+            this.Distance = distance;
+        }
+
+        public int Distance
+        {
+            get { return _Distance; }
+            set { _Distance = value; }
+        }
+        public int Money()
+        {
+            int won = Distance*2200;
+            return won;
+        }
+        public double MileTransform()
+        {
+            double mile = Distance / 1.6;
+            return mile;
+        }
+    }
+
+    public interface ICalc
+    {
+        int Money();
+        double MileTransform();
+    }
+}
---
+++ midterm/ksb254/ksb254/문제2.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ksb254
+{
+    class 문제2
+    {
+        static void Main(string[] args)
+        {
+            int won = 0;
+            Console.Write("물건의 가격을 입력하세요(1000원 미만) : ");
+            won = Convert.ToInt32(Console.ReadLine());
+
+            int fivehun=0, hun=0, fifty=0, ten=0, one = 0;
+            int charge = 1000 - won;
+
+            if(charge >= 500)
+            {
+                fivehun = charge / 500;
+                charge = charge % 500;
+            }
+            if (charge >= 100)
+            {
+                hun = charge / 100;
+                charge = charge % 100;
+            }
+            if(charge >= 50)
+            {
+                fifty = charge / 50;
+                charge = charge % 50;
+            }
+            if(charge >= 10)
+            {
+                ten = charge / 10;
+                charge = charge % 10;
+            }
+            one = charge;
+
+            Console.WriteLine("거스름돈입니다.");
+            Console.WriteLine("");
+
+            Console.WriteLine("500원 {0} 개", fivehun);
+            Console.WriteLine("100원 {0} 개", hun);
+            Console.WriteLine("50원 {0} 개", fifty);
+            Console.WriteLine("10원 {0} 개", ten);
+            Console.WriteLine("1원 {0} 개", one);
+            Console.WriteLine("거스름돈은 {0}원 입니다.",1000-won);
+        }
+    }
+}
